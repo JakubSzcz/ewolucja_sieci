8bae3ed945b096b509f694a7f56c88b30c6a639b
Jakub Szczudlik <jszczudlik@student.agh.edu.pl>
2024-04-20 19:31:19 +0200

    tiny adjustments

--- Python/main.py
+++ Python/main.py
@@ -2,12 +2,12 @@ import numpy as np
 import scipy.io
 
 # Load the .mat file
-mat = scipy.io.loadmat('results.mat')
+mat = scipy.io.loadmat("../Matlab/results.mat")
 
 # Check what variables are in the .mat file
-#print(mat.keys())
+print(mat.keys())
 
-rssi_values_list = np.array(np.double(mat['RSSI'][0]))
+rssi_values_list = np.array(mat['RSSI'], dtype=np.complex_)
 
 for index, value in enumerate(rssi_values_list, start=1):
     print(f'RSSI{index}: {value}')
