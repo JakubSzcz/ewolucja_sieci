f3d5f4e598f0cca31df6a84f28850acf366d31be
Jakub Szczudlik <jszczudlik@student.agh.edu.pl>
2024-04-20 20:34:50 +0200

    basic model made with keras

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,20 @@
 import numpy as np
 import scipy.io
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
-# Load the .mat file
+# load data
 mat = scipy.io.loadmat("../Matlab/results.mat")
+rssi_values_matrix = np.array(mat['RSSI'], dtype=np.double)
+people_position = np.array(mat['pepolePosMatrix'], dtype=np.double)[:, :2]
 
-# Check what variables are in the .mat file
-print(mat.keys())
+# implement Keras model
+model = Sequential()
+model.add(Dense(8, activation='relu'))
+model.add(Dense(2, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(rssi_values_matrix, people_position, epochs=150, batch_size=1, verbose=2)
 
-rssi_values_list = np.array(mat['RSSI'], dtype=np.complex_)
-
-for index, value in enumerate(rssi_values_list, start=1):
-    print(f'RSSI{index}: {value}')
+# evaluate
+loss, accuracy = model.evaluate(rssi_values_matrix, people_position, verbose=0)
+print('Accuracy: {:.2f}%'.format(accuracy * 100))
