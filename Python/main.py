4db8d986d280187a986df22089350db93cf0ee40
jakub.niecko@motorolasolutions.com <jakub.niecko@motorolasolutions.com>
2024-04-22 21:01:26 +0200

    update main.py

--- Python/main.py
+++ Python/main.py
@@ -7,20 +7,25 @@ from tensorflow.keras.models import Sequential
 
 # load data
 dir_path = os.getcwd()
-mat = scipy.io.loadmat(fr"{dir_path}/Matlab/training_set.mat")
+mat = scipy.io.loadmat(fr"{dir_path}/Matlab/people10000_person.mat")
 rssi_values_matrix = np.array(mat['RSSI'], dtype=np.double)
 people_position = np.array(mat['positions'], dtype=np.double)[:, :2]
 
-print(rssi_values_matrix)
-print(people_position)
+print(rssi_values_matrix.shape)
+print(people_position.shape)
+
 
 # implement Keras model
 model = Sequential()
 model.add(Dense(8, activation='relu'))
-model.add(Dense(2, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(rssi_values_matrix, people_position, epochs=150, batch_size=1, verbose=2)
+model.add(Dense(2, activation='softmax'))
+#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#model.fit(rssi_values_matrix, people_position, epochs=150, batch_size=1, verbose=2)
 
+model.summary()
+
+model.compile(optimizer='adam', metrics=['accuracy'], loss = "categorical_crossentropy")
+model.fit(x=rssi_values_matrix, y=people_position, epochs=150, batch_size=10, verbose=1)
 # evaluate
-loss, accuracy = model.evaluate(rssi_values_matrix, people_position, verbose=0)
-print('Accuracy: {:.2f}%'.format(accuracy * 100))
+#loss, accuracy = model.evaluate(rssi_values_matrix, people_position, verbose=0)
+#print('Accuracy: {:.2f}%'.format(accuracy * 100))
